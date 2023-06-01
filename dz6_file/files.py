19a9a4573ffb75de7e19e063bda795c7e0372ad9
shurik1777 <shurik1777@gmail.com>
2023-06-01 22:01:51 +0300

    add classes in jurnal2

--- dz6_file/files.py
+++ dz6_file/files.py
@@ -6,15 +6,22 @@
 with open("sample.txt", "r", encoding='UTF=8') as file:
     text = file.read()
 
-# разбиваем текст на предложения по точкам и удаляем лишние пробелы
-sentences = [str_.strip() for str_ in text.split('.')]
+# # разбиваем текст на предложения по точкам и удаляем лишние пробелы
+# sentences = [str_.strip() for str_ in text.split('.')]
+#
+# # преобразуем первую букву каждого предложения в заглавную
+# result = []
+# for str_ in sentences:
+#     if len(str_) > 0:
+#         result.append(str_[0].upper() + str_[1:])
+#
+# # соединяем предложения обратно в текст и записываем его в файл
+# with open("sample2.txt", "w", encoding='UTF=8') as file:
+#     file.write('. '.join(result))
+#  еще вариант без срезов с лямбдой
+text = text.split('.')
+text = list(map(lambda x: x.strip(), text))
 
-# преобразуем первую букву каждого предложения в заглавную
-result = []
-for str_ in sentences:
-    if len(str_) > 0:
-        result.append(str_[0].upper() + str_[1:])
-
-# соединяем предложения обратно в текст и записываем его в файл
-with open("sample2.txt", "w", encoding='UTF=8') as file:
-    file.write('. '.join(result))
+with open("sample.txt", "w", encoding='UTF=8') as file:
+    text = '. '.join([sentence.capitalize() for sentence in text])
+    file.write(text)
--- dz6_file/files_2.py
+++ dz6_file/files_2.py
@@ -1,16 +1,49 @@
-"""2. Выдать топ 5 самых встречаемых слов в Алисе
-файл 'Alice_in_Wonderland.txt'
-длинна которых не меньше 5 букв.
-Так же можете собрать еще какую-то статистику
-(не обязательное задание на фантазию)."""
-from string import punctuation
-with open("Alice_in_Wonderland.txt", "r", encoding='UTF=8') as file:
-    text = file.read()
-# print(text)
-# проверка длины слов и добавления их в список
-"""result = []
-words = text.split()
-for word in words:
-    if len(word) > 5:
-        result.append(word)
-print(result)"""
+def print_board(board):  #  доска
+    for row in board:
+        print(" ".join(row))
+
+def is_safe(board, row, col):
+    # Проверяем строку
+    for i in range(len(board)):
+        if board[row][i] == "Q":
+            return False
+
+    # Проверяем столбец
+    for i in range(len(board)):
+        if board[i][col] == "Q":
+            return False
+
+    # Проверяем диагонали
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if (i + j == row + col) or (i - j == row - col):
+                if board[i][j] == "Q":
+                    return False
+
+    return True
+
+def solve(board, col):
+    # Если все ферзи успешно размещены, то выводим доску
+    if col == len(board):
+        print_board(board)
+        print("\n")
+        return True
+
+    solved = False
+
+    # Выбираем строку для ферзя в текущем столбце
+    for row in range(len(board)):
+        if is_safe(board, row, col):
+            board[row][col] = "Q"
+
+            # Рекурсивно размещаем оставшиеся ферзи на доске
+            solved = solve(board, col + 1) or solved
+
+            # Удаляем ферзя, если он не решил проблему для оставшихся ферзей
+            board[row][col] = "_"
+
+    return solved
+
+board = [["." for _ in range(8)] for _ in range(8)]
+
+solve(board, 0)
